fc6bb6b4ed7493a21bbe25354e7a2188a4f6159e
Kruthinayak <kruthinayaka07@gmail.com>
2026-08-11 22:17:49 +0530

    Minion Game

---
+++ 08_strings/13_Minion_game/solution.py
@@ -0,0 +1,17 @@
+s = input()
+
+kevin = 0
+stuart = 0
+
+for i in range(len(s)):
+    if s[i] in "AEIOU":
+        kevin += len(s) - i
+    else:
+        stuart += len(s) - i
+
+if kevin > stuart:
+    print("Kevin", kevin)
+elif stuart > kevin:
+    print("Stuart", stuart)
+else:
+    print("Draw")
