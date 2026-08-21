2cab29ed13d54f080fc6ce92f968caf058b8bbaf
Kruthinayak <kruthinayaka07@gmail.com>
2026-08-21 21:50:47 +0530

    Integers come in all sizes

---
+++ 11_Math/02_Integers_come_in_All_sizes/solution.py
@@ -0,0 +1,17 @@
+# Read the first number
+a = int(input())
+
+# Read the second number
+b = int(input())
+
+# Read the third number
+c = int(input())
+
+# Read the fourth number
+d = int(input())
+
+# Calculate a^b + c^d
+result = a ** b + c ** d
+
+# Print the result
+print(result)
